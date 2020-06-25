f8551f65708201b51fac23ca100e06d49ebe8a85
Soprativlenie <zlykabober84@gmail.com>
2020-06-25 18:26:45 +0300

    Added the initialisation of elements

--- src/test/java/com/jelvix/pages/pageblocks/ContactUsForm.java
+++ src/test/java/com/jelvix/pages/pageblocks/ContactUsForm.java
@@ -4,11 +4,13 @@ import com.jelvix.pages.Page;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class ContactUsForm extends Page {
 
     public ContactUsForm(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     @FindBy(xpath = "//input[@type='text']")
