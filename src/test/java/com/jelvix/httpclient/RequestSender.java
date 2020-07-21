a09c171b2f503ca5aade8ef42feed7b7b480dc57
Soprativlenie <zlykabober84@gmail.com>
2020-07-21 18:20:27 +0300

    Minor changes

--- src/test/java/com/jelvix/httpclient/RequestSender.java
+++ src/test/java/com/jelvix/httpclient/RequestSender.java
@@ -1,29 +1,41 @@
 package com.jelvix.httpclient;/* Created by user on 20.07.20 */
 
-import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.openqa.selenium.WebElement;
 
 import java.io.IOException;
+import java.util.List;
 
 public class RequestSender {
-    private CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    public boolean isImagesLinkValid(WebElement element) {
-        HttpGet request = new HttpGet(element.getAttribute("src"));
-        try {
-            HttpResponse response = httpClient.execute(request);
-            if (response.getStatusLine().getStatusCode() != 200) {
-                System.out.println(request);
-                System.out.println(response.getStatusLine().getStatusCode());
-                System.out.println(response.getStatusLine().getReasonPhrase());
-                return false;
+
+    public boolean isImagesLinkValid(List<WebElement> elements) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        int testCounter = 1;
+        for (WebElement imgElement : elements) {
+            try {
+                HttpGet requestByLink = new HttpGet(imgElement.getAttribute("src"));
+                CloseableHttpResponse response = httpClient.execute(requestByLink);
+                try {
+                    System.out.println(testCounter + " " + requestByLink + " " + response.getStatusLine().getStatusCode());
+                    testCounter++;// Debug print
+                    if (response.getStatusLine().getStatusCode() != 200) {
+                        System.out.println(response.getStatusLine().getStatusCode());
+                        System.out.println(requestByLink);
+                        return false;
+                    }
+                    response.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        httpClient.close();
         return true;
     }
 }
