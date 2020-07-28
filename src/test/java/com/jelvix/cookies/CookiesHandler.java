9a604167f48a227dae6eaeed1e2f1c2a17f0eff9
Soprativlenie <zlykabober84@gmail.com>
2020-07-28 13:08:36 +0300

    Made minor changes

--- src/test/java/com/jelvix/cookies/CookiesHandler.java
+++ src/test/java/com/jelvix/cookies/CookiesHandler.java
@@ -6,11 +6,11 @@ import javax.xml.bind.DatatypeConverter;
 import java.util.Date;
 
 public class CookiesHandler {
-    private String cookiePath = "/";
-    private Date cookieExpires = DatatypeConverter.parseDateTime("2021-01-01T12:00:00Z").getTime();
-    private Cookie closeSubscribeCookie = new Cookie("close_subscribe", "true", cookiePath, cookieExpires);
-    private Cookie closeCovidCookie = new Cookie("close_covid", "true", cookiePath, cookieExpires);
-    private Cookie cookieNoticeAcceptedCookie = new Cookie("cookie_notice_accepted", "true", cookiePath, cookieExpires);
+    private String cookiePathValue = "/";
+    private Date cookieExpiresValue = DatatypeConverter.parseDateTime("2021-01-01T12:00:00Z").getTime();
+    private Cookie closeSubscribeCookie = new Cookie("close_subscribe", "true", cookiePathValue, cookieExpiresValue);
+    private Cookie closeCovidCookie = new Cookie("close_covid", "true", cookiePathValue, cookieExpiresValue);
+    private Cookie cookieNoticeAcceptedCookie = new Cookie("cookie_notice_accepted", "true", cookiePathValue, cookieExpiresValue);
 
     public Cookie getCloseSubscribeCookie() {
         return closeSubscribeCookie;
