c23c99311e3d7431f8ebb338a546d8f3797f9f4f
Soprativlenie <zlykabober84@gmail.com>
2020-08-26 17:17:23 +0300

    Added new tests to the Smoke Suite

--- src/test/java/io/wowemail/tests/Emails.java
+++ src/test/java/io/wowemail/tests/Emails.java
@@ -1,33 +1,29 @@
 package io.wowemail.tests;/* Created by user on 17.08.20 */
 
-import com.jayway.jsonpath.JsonPath;
+
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
-import java.util.List;
-
 
 public class Emails {
-    private final String url = "https://mailinator.com/api/v2/domains/public/inboxes/";
-    private final String authorizationToken = "f836444904244057ab4d37030047f038";
-    private final String sender = "wowemail.io";
-    private final String fromFull = "support@wowemail.io";
-    private final String theWelcomeMessageSubject = "Welcome to WowEmail!";
-    private final String theOrderMessageSubject = "Great news! We’ve got your order";
+    private String baseFlockUrl;
+    private String session;
 
-
-    public boolean isWelcomeLetterReceives(String inbox) throws UnirestException {
-        String responseWithLetterList = Unirest.get(url + inbox)
-                .header("Authorization", authorizationToken)
+    public void getMessageFromTheSupportWowEmailMailBox() throws UnirestException {
+        String responseFromTheLoginFlock = Unirest.post("https://api.flockmail.com/r/mailSessionInfo")
+                .queryString("email", "support@wowemail.io")
+                .queryString("password", "WXcTnijDPJ")
+                .queryString("installation_id", "Chrome-browser-1597402721991")
                 .asString()
                 .getBody();
+    }
 
-        List<String> subjectsList = JsonPath.read(responseWithLetterList, ".msgs[*].subject");
-        for (String subject : subjectsList) {
-            if (subject.equals(theWelcomeMessageSubject)) {
-                return true;
-            }
-        }
-        return false;
+    public String LoginToTheFlockMail() throws UnirestException {
+        return Unirest.post("https://api.flockmail.com/r/mailSessionInfo")
+                .queryString("email", "support@wowemail.io")
+                .queryString("password", "WXcTnijDPJ")
+                .queryString("installation_id", "Chrome-browser-1597402721991")
+                .asString()
+                .getBody();
     }
 }
--- src/test/java/io/wowemail/tests/SmokeTest.java
+++ src/test/java/io/wowemail/tests/SmokeTest.java
@@ -1,13 +1,39 @@
 package io.wowemail.tests;/* Created by user on 14.08.20 */
 
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SmokeTest extends TestBase {
 
-    @Test
-    public void should_verifyThatPreviewImageIsDisplaying_WhenUserIsOnTheMainPage(){
+    @Test(enabled = false, description = "Find out with display images inside slider bar")
+    public void should_DisplayAllImages_WhenUserIsOnTheMainPage(){
         mainPage.open();
         Assert.assertTrue(mainPage.AreAllImagesDisplayingOnTheMainPage());
     }
+    @Test
+    public void should_ShowSuccessPopup_When_TheUserFillsTheGetInTouchWithUsFormOnTheMainPage(){
+        mainPage.open()
+                .enterTheNameToTheNameField("Test")
+                .enterTheEmailToTheEmailField("test@jelxiv.com")
+                .enterTheMessageToTheMessageField("Test Message")
+                .clickTheSendButton();
+        Assert.assertTrue(mainPage.isSuccessSendingPopupDisplay());
+    }
+
+    @Test
+    public void should_DeliverTheMessageToTheInfoWowEmailMailBox_When_TheUserSendsGetInTouchFormTheMainPage() throws UnirestException {
+        mainPage.open()
+                .enterTheNameToTheNameField("John Doe421")
+                .enterTheEmailToTheEmailField("test@jelvix.com")
+                .enterTheMessageToTheMessageField("Test Message")
+                .clickTheSendButton();
+        mainPage.isSuccessSendingPopupDisplay();
+        mainPage.clickCloseButtonOnTheSuccessSendingOfTheGetInTouchPopup();
+        System.out.println(httpSender.getDataOfTheFirstMessage());
+
+
+    }
+
+
 }
