a6f5b01ee67dc3b35e0c8f1bf5b002bd13c866d7
Soprativlenie <zlykabober84@gmail.com>
2020-08-26 17:16:16 +0300

    Added the WebElemnts to MainPage Object

--- src/test/java/io/wowemail/pages/MainPage.java
+++ src/test/java/io/wowemail/pages/MainPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -20,6 +21,30 @@ public class MainPage extends Page {
 
     @FindBy(xpath = "//img")
     private List<WebElement> images;
+
+    @FindBy(xpath = "//div[@class='main-text']/a")
+    private WebElement orderNowButton;
+
+    @FindBy(id = "name-input")
+    private WebElement nameInput;
+
+    @FindBy(id = "email-input")
+    private WebElement emailInput;
+
+    @FindBy(id = "message-input")
+    private WebElement messageInput;
+
+    @FindBy(xpath = "//button/span[text()='Send']")
+    private WebElement sendButton;
+
+    @FindBy(id = "thanks-sent-popup")
+    private WebElement successSendingOfGetInTouchFormPopup;
+
+    @FindBy(xpath = "//div[@id='thanks-sent-popup']//button[@title='Close']")
+    private WebElement closeIconOnTheSuccessSendingOfGetInTouchPopup;
+
+    @FindBy(xpath = "//div[@id='thanks-sent-popup']//button[text()='Close']")
+    private WebElement closeButtonOnTheSuccessSendingOfGetInTouchPopup;
 
     public MainPage open() {
         driver.navigate().to(Page.host + PageRoutes.MAIN.getRoute());
@@ -39,5 +64,39 @@ public class MainPage extends Page {
             System.out.println(image.getAttribute("src"));
         }
         return true;
+    }
+
+    public MainPage enterTheNameToTheNameField(String name) {
+        nameInput.sendKeys(name);
+        return this;
+    }
+
+    public MainPage enterTheEmailToTheEmailField(String email) {
+        emailInput.sendKeys(email);
+        return this;
+    }
+
+    public MainPage enterTheMessageToTheMessageField(String message) {
+        messageInput.sendKeys(message);
+        return this;
+    }
+
+    public void clickTheSendButton() {
+        sendButton.click();
+    }
+
+    public boolean isSuccessSendingPopupDisplay() {
+        wait.withMessage("Thanks for getting in touch! pop up isn't displaying").until(ExpectedConditions.visibilityOf(successSendingOfGetInTouchFormPopup));
+        return successSendingOfGetInTouchFormPopup.isDisplayed();
+    }
+
+    public MainPage clickCloseIconOnTheSuccessSendingOfTheGetInTouchPopup() {
+        closeIconOnTheSuccessSendingOfGetInTouchPopup.click();
+        return this;
+    }
+
+    public MainPage clickCloseButtonOnTheSuccessSendingOfTheGetInTouchPopup() {
+        closeButtonOnTheSuccessSendingOfGetInTouchPopup.click();
+        return this;
     }
 }
